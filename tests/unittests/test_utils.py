200aac173eb1ac5654e226d851b6f1b5ec80ee7b
Shachi Agarwal <sagarwal@arista.com>
2023-04-14 06:37:36 -0600

    Issue #130: Add unittests for utils.py

---
+++ tests/unittests/test_utils.py
@@ -0,0 +1,32 @@
+""" Tests for vane.utils functions """
+from collections.abc import Iterable
+
+import pytest
+import vane.utils
+
+
+def test_make_iterable_from_string():
+    """ Verify string is made iterable """
+    result = vane.utils.make_iterable("test")
+    assert isinstance(result, Iterable)
+    assert len(result) == 1
+
+
+def test_make_iterable_from_unicode():
+    """ Verify unicode string is made iterable """
+    result = vane.utils.make_iterable("test")
+    assert isinstance(result, Iterable)
+    assert len(result) == 1
+
+
+def test_make_iterable_from_iterable():
+    """ Verify list is made iterable """
+    result = vane.utils.make_iterable(["test"])
+    assert isinstance(result, Iterable)
+    assert len(result) == 1
+
+
+def test_make_iterable_raises_type_error():
+    """ Verify type error is raised """
+    with pytest.raises(TypeError):
+        vane.utils.make_iterable(object())

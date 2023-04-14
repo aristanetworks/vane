3ff8b19af81807e973b7c8f0682e7075404ca85d
Shachi Agarwal <sagarwal@arista.com>
2023-04-14 06:38:52 -0600

    Issue #130: Fix black formatting

--- tests/unittests/test_utils.py
+++ tests/unittests/test_utils.py
@@ -6,27 +6,27 @@ import vane.utils
 
 
 def test_make_iterable_from_string():
-    """ Verify string is made iterable """
+    """Verify string is made iterable"""
     result = vane.utils.make_iterable("test")
     assert isinstance(result, Iterable)
     assert len(result) == 1
 
 
 def test_make_iterable_from_unicode():
-    """ Verify unicode string is made iterable """
+    """Verify unicode string is made iterable"""
     result = vane.utils.make_iterable("test")
     assert isinstance(result, Iterable)
     assert len(result) == 1
 
 
 def test_make_iterable_from_iterable():
-    """ Verify list is made iterable """
+    """Verify list is made iterable"""
     result = vane.utils.make_iterable(["test"])
     assert isinstance(result, Iterable)
     assert len(result) == 1
 
 
 def test_make_iterable_raises_type_error():
-    """ Verify type error is raised """
+    """Verify type error is raised"""
     with pytest.raises(TypeError):
         vane.utils.make_iterable(object())

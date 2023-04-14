7eb2ccfd50d50cb4f9ac8fa3deb0d28787faac6b
Shachi Agarwal <sagarwal@arista.com>
2023-04-14 09:41:01 -0600

    Issue #130: Add more utils test

--- tests/unittests/test_utils.py
+++ tests/unittests/test_utils.py
@@ -5,28 +5,39 @@ import pytest
 import vane.utils
 
 
-def test_make_iterable_from_string():
-    """Verify string is made iterable"""
-    result = vane.utils.make_iterable("test")
-    assert isinstance(result, Iterable)
-    assert len(result) == 1
+class UtilsTests:
+    """Set of tests for vane/utils.py"""
 
+    def test_make_iterable_from_string(self):
+        """Verify string is made iterable"""
+        result = vane.utils.make_iterable("test")
+        assert isinstance(result, Iterable)
+        assert len(result) == 1
 
-def test_make_iterable_from_unicode():
-    """Verify unicode string is made iterable"""
-    result = vane.utils.make_iterable("test")
-    assert isinstance(result, Iterable)
-    assert len(result) == 1
+    def test_make_iterable_from_unicode(self):
+        """Verify unicode string is made iterable"""
+        result = vane.utils.make_iterable("test")
+        assert isinstance(result, Iterable)
+        assert len(result) == 1
 
+    def test_make_iterable_from_iterable(self):
+        """Verify list is made iterable"""
+        result = vane.utils.make_iterable(["test"])
+        assert isinstance(result, Iterable)
+        assert len(result) == 1
 
-def test_make_iterable_from_iterable():
-    """Verify list is made iterable"""
-    result = vane.utils.make_iterable(["test"])
-    assert isinstance(result, Iterable)
-    assert len(result) == 1
+    def test_make_iterable_raises_type_error(self):
+        """Verify type error is raised"""
+        with pytest.raises(TypeError):
+            vane.utils.make_iterable(object())
 
+    def test_get_current_fixture_testclass(self, request):
+        """Verify get current fixture testclass"""
+        print(request)
+        result = vane.utils.get_current_fixture_testclass(request)
+        assert result == "UtilsTests"
 
-def test_make_iterable_raises_type_error():
-    """Verify type error is raised"""
-    with pytest.raises(TypeError):
-        vane.utils.make_iterable(object())
+    def test_get_current_fixture_testname(self, request):
+        """Verify get current fixture testclass"""
+        result = vane.utils.get_current_fixture_testname(request)
+        assert result == "test_get_current_fixture_testname"

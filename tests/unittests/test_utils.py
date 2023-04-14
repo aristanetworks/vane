44d7e3aeececf4ba7ade27ae6cdd1ed93625bb87
Shachi Agarwal <sagarwal@arista.com>
2023-04-14 09:45:31 -0600

    Issue #130: Fix import

--- tests/unittests/test_utils.py
+++ tests/unittests/test_utils.py
@@ -2,7 +2,7 @@
 from collections.abc import Iterable
 
 import pytest
-import vane.utils
+from vane.utils import make_iterable, get_current_fixture_testclass, get_current_fixture_testname
 
 
 class UtilsTests:
@@ -10,34 +10,34 @@ class UtilsTests:
 
     def test_make_iterable_from_string(self):
         """Verify string is made iterable"""
-        result = vane.utils.make_iterable("test")
+        result = make_iterable("test")
         assert isinstance(result, Iterable)
         assert len(result) == 1
 
     def test_make_iterable_from_unicode(self):
         """Verify unicode string is made iterable"""
-        result = vane.utils.make_iterable("test")
+        result = make_iterable("test")
         assert isinstance(result, Iterable)
         assert len(result) == 1
 
     def test_make_iterable_from_iterable(self):
         """Verify list is made iterable"""
-        result = vane.utils.make_iterable(["test"])
+        result = make_iterable(["test"])
         assert isinstance(result, Iterable)
         assert len(result) == 1
 
     def test_make_iterable_raises_type_error(self):
         """Verify type error is raised"""
         with pytest.raises(TypeError):
-            vane.utils.make_iterable(object())
+            make_iterable(object())
 
     def test_get_current_fixture_testclass(self, request):
         """Verify get current fixture testclass"""
         print(request)
-        result = vane.utils.get_current_fixture_testclass(request)
+        result = get_current_fixture_testclass(request)
         assert result == "UtilsTests"
 
     def test_get_current_fixture_testname(self, request):
         """Verify get current fixture testclass"""
-        result = vane.utils.get_current_fixture_testname(request)
+        result = get_current_fixture_testname(request)
         assert result == "test_get_current_fixture_testname"

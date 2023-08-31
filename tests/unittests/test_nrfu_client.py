4c698c08565c8de41e6df901e49d84ad2f70c061
Rewati Shitole <rewati@arista.com>
2023-08-31 09:57:00 -0700

    Added more tests

--- tests/unittests/test_nrfu_client.py
+++ tests/unittests/test_nrfu_client.py
@@ -141,13 +141,17 @@ def test_cvp_application(mocker, loginfo, capsys):
     mocker.patch("vane.nrfu_client.NrfuClient.setup")
     client = nrfu_client.NrfuClient()
 
-    mocker.patch(
+    mocker_object = mocker.patch(
         "vane.nrfu_client.NrfuClient.get_duts_data", return_value=["hostname", "ipaddress"]
     )
     device_data, source = client.cvp_application()
 
     # Capture the printed output
     captured = capsys.readouterr()
+
+    # Assert that the expected calls were made in the specified order
+    expected_calls = [call("127.0.0.1")]
+    assert mocker_object.call_args_list == expected_calls
 
     # Assert that the expected content is present in the captured output
     expected_content = "Using CVP to gather duts data"
@@ -158,10 +162,229 @@ def test_cvp_application(mocker, loginfo, capsys):
 
     loginfo.assert_called_with("Using CVP to gather duts data")
 
-    # def test_not_cvp_application():
-    #     pass
-    # def test_get_duts_data():
-    #     pass
+
+def test_not_cvp_application_local_cvp(mocker):
+    """Testing functionality of generating device data when connecting
+    to CVP locally"""
+
+    mocker.patch("vane.nrfu_client.NrfuClient.setup")
+    client = nrfu_client.NrfuClient()
+
+    mocker_object = mocker.patch("builtins.input")
+    mocker_object.side_effect = [
+        "yes",
+        "10.255.31.187",
+    ]
+
+    expected_data = [["host1", "10.255.31.188"], ["host2", "10.255.31.189"]]
+    mocker_object = mocker.patch(
+        "vane.nrfu_client.NrfuClient.get_duts_data", return_value=expected_data
+    )
+    device_data, source = client.not_cvp_application()
+
+    # Assert that the expected calls were made in the specified order
+    expected_calls = [call("10.255.31.187")]
+    assert mocker_object.call_args_list == expected_calls
+
+    assert source == "cvp"
+    assert device_data == expected_data
+
+
+def test_not_cvp_application_device_file(mocker):
+    """Testing functionality of generating device data when reading
+    from device ip file"""
+
+    mocker.patch("vane.nrfu_client.NrfuClient.setup")
+    client = nrfu_client.NrfuClient()
+
+    expected_data = ["10.255.31.184", "10.255.31.185", "10.255.31.186", "10.255.31.187"]
+    mocker.patch("builtins.input", return_value="no")
+    mocker.patch("vane.nrfu_client.prompt", return_value="tests/unittests/fixtures/device_ip_file")
+    mocker_object = mocker.patch(
+        "vane.nrfu_client.NrfuClient.is_valid_text_file", return_value=True
+    )
+    mocker_object_two = mocker.patch(
+        "vane.nrfu_client.NrfuClient.read_device_list_file", return_value=expected_data
+    )
+    device_data, source = client.not_cvp_application()
+
+    # Assert that the expected calls were made in the specified order
+    expected_calls = [call("tests/unittests/fixtures/device_ip_file")]
+    assert mocker_object.call_args_list == expected_calls
+    assert mocker_object_two.call_args_list == expected_calls
+
+    assert source == "non-cvp"
+    assert device_data == expected_data
+
+
+def test_not_cvp_application_invalid_choice(mocker):
+    """Testing functionality which handles user choice of y/yes/n/no"""
+
+    mocker.patch("vane.nrfu_client.NrfuClient.setup")
+    client = nrfu_client.NrfuClient()
+
+    mocker_object = mocker.patch("builtins.input")
+    mocker_object.side_effect = [
+        "KKK",
+        "yes",
+        "10.255.31.187",
+    ]
+
+    expected_data = [["host1", "10.255.31.188"], ["host2", "10.255.31.189"]]
+    mocker_object_two = mocker.patch(
+        "vane.nrfu_client.NrfuClient.get_duts_data", return_value=expected_data
+    )
+    device_data, source = client.not_cvp_application()
+
+    assert mocker_object.call_count == 3
+    # Assert that the expected calls were made in the specified order
+    expected_calls = [call("10.255.31.187")]
+    assert mocker_object_two.call_args_list == expected_calls
+
+    assert source == "cvp"
+    assert device_data == expected_data
+
+
+def test_not_cvp_application_invalid_file(mocker):
+    """Testing functionality of generating device data when reading
+    from device ip file"""
+
+    mocker.patch("vane.nrfu_client.NrfuClient.setup")
+    client = nrfu_client.NrfuClient()
+
+    expected_data = ["10.255.31.184", "10.255.31.185", "10.255.31.186", "10.255.31.187"]
+    mocker.patch("builtins.input", return_value="no")
+    mocker_object = mocker.patch("vane.nrfu_client.prompt")
+    mocker_object.side_effect = [
+        "tests/unittests/fixtures/",
+        "tests/unittests/fixtures/device_ip_file",
+    ]
+    mocker_object_two = mocker.patch("vane.nrfu_client.NrfuClient.is_valid_text_file")
+    mocker_object_two.side_effect = [
+        False,
+        True,
+    ]
+    mocker_object_three = mocker.patch(
+        "vane.nrfu_client.NrfuClient.read_device_list_file", return_value=expected_data
+    )
+    device_data, source = client.not_cvp_application()
+
+    # Assert that the expected calls were made in the specified order
+    expected_calls = [
+        call("tests/unittests/fixtures/"),
+        call("tests/unittests/fixtures/device_ip_file"),
+    ]
+    assert mocker_object_two.call_args_list == expected_calls
+    expected_calls = [call("tests/unittests/fixtures/device_ip_file")]
+    assert mocker_object_three.call_args_list == expected_calls
+
+    assert mocker_object.call_count == 2
+    assert mocker_object_two.call_count == 2
+    assert source == "non-cvp"
+    assert device_data == expected_data
+
+
+def test_get_duts_data(mocker, loginfo):
+    """Testing functionality to get and process data from CVP"""
+    inventory_data = [
+        {
+            "modelName": "DCS-7050SX3-48YC12",
+            "internalVersion": "4.28.7M",
+            "systemMacAddress": "74:83:ef:78:7d:64",
+            "bootupTimestamp": 0,
+            "version": "4.28.7M",
+            "architecture": "",
+            "internalBuild": "",
+            "hardwareRevision": "",
+            "domainName": "rtp-pslab.com",
+            "hostname": "ps-rtp1-leaf1",
+            "fqdn": "ps-rtp1-leaf1.rtp-pslab.com",
+            "serialNumber": "JPE18380270",
+            "deviceType": "eos",
+            "danzEnabled": False,
+            "mlagEnabled": False,
+            "streamingStatus": "active",
+            "parentContainerKey": "undefined_container",
+            "status": "Registered",
+            "complianceCode": "0000",
+            "complianceIndication": "",
+            "ztpMode": False,
+            "unAuthorized": False,
+            "ipAddress": "10.88.160.164",
+            "key": "74:83:ef:78:7d:64",
+            "deviceInfo": "Registered",
+            "deviceStatus": "Registered",
+            "isMLAGEnabled": False,
+            "isDANZEnabled": False,
+            "parentContainerId": "undefined_container",
+            "bootupTimeStamp": 0,
+            "internalBuildId": "",
+            "taskIdList": [],
+            "tempAction": None,
+            "memTotal": 0,
+            "memFree": 0,
+            "sslConfigAvailable": False,
+            "sslEnabledByCVP": False,
+            "lastSyncUp": 0,
+            "type": "netelement",
+            "dcaKey": None,
+            "containerName": "Undefined",
+        },
+        {
+            "modelName": "CCS-720XP-48ZC2",
+            "internalVersion": "4.28.7M",
+            "systemMacAddress": "fc:bd:67:0f:33:f5",
+            "bootupTimestamp": 0,
+            "version": "4.28.7M",
+            "architecture": "",
+            "internalBuild": "",
+            "hardwareRevision": "",
+            "domainName": "rtp-pslab.com",
+            "hostname": "ps-rtp1-host3",
+            "fqdn": "ps-rtp1-host3.rtp-pslab.com",
+            "serialNumber": "JPE19161323",
+            "deviceType": "eos",
+            "danzEnabled": False,
+            "mlagEnabled": False,
+            "streamingStatus": "active",
+            "parentContainerKey": "undefined_container",
+            "status": "Registered",
+            "complianceCode": "0000",
+            "complianceIndication": "",
+            "ztpMode": False,
+            "unAuthorized": False,
+            "ipAddress": "10.88.160.69",
+            "key": "fc:bd:67:0f:33:f5",
+            "deviceInfo": "Registered",
+            "deviceStatus": "Registered",
+            "isMLAGEnabled": False,
+            "isDANZEnabled": False,
+            "parentContainerId": "undefined_container",
+            "bootupTimeStamp": 0,
+            "internalBuildId": "",
+            "taskIdList": [],
+            "tempAction": None,
+            "memTotal": 0,
+            "memFree": 0,
+            "sslConfigAvailable": False,
+            "sslEnabledByCVP": False,
+            "lastSyncUp": 0,
+            "type": "netelement",
+            "dcaKey": None,
+            "containerName": "Undefined",
+        },
+    ]
+    mocker.patch("vane.nrfu_client.NrfuClient.setup")
+    client = nrfu_client.NrfuClient()
+
+
+    mocker_object = mocker.patch("vane.nrfu_client.CvpClient")
+    mocker.patch("vane.nrfu_client.CvpClient.connect")
+    client.get_duts_data("10.255.31.186")
+
+    loginfo.assert_called_with("Connecting to CVP to gather duts data")
+
+
 
 
 def test_read_device_list_file(mocker, loginfo):
@@ -179,10 +402,11 @@ def test_read_device_list_file(mocker, loginfo):
 
     loginfo.assert_called_with("Reading in dut ip data from device list file")
 
-    # def test_generate_duts_file():
-    #     pass
-    # def test_generate_definitions_file():
-    #     pass
+
+# def test_generate_duts_file():
+#     pass
+# def test_generate_definitions_file():
+#     pass
 
 
 def test_is_valid_text_file(mocker):

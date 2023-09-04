8f7c7044c8c5af52b613bbb795f2a9e70577929f
Abhijeetsingh <abhijeetsingh.thakur@gslab.com>
2023-09-04 15:57:03 +0530

    Issue-419: Updated the testcase verification using the loop

--- nrfu_tests/test_base_services_dns.py
+++ nrfu_tests/test_base_services_dns.py
@@ -22,42 +22,6 @@ class DnsBaseServicesTests:
     Testcases for verification of DNS base services.
     """
 
-    def verify_dns(self, vx_name_servers, ip_version, ipvx_fail_if_not_configured, dut, tops):
-        """
-        Utility function for the verification of DNS functionality.
-        Args:
-            vx_name_servers(String): IP address of the name server.
-            ipvx_fail_if_not_configured(boolean):
-            dut(dict): details related to a particular device
-            tops(dict): Testops class object
-        Returns:
-            dict: Actual output for verification
-        """
-        ip_version = ip_version.split("_")[0]
-        actual_output = {}
-
-        try:
-            if ipvx_fail_if_not_configured:
-                assert (
-                    vx_name_servers
-                ), f"For {ip_version}, name-server is not configured on device{dut['name']}.\n"
-
-            reverse_resolution_ip = vx_name_servers[0]
-            bash_cmd = f"bash timeout 10 nslookup {reverse_resolution_ip}"
-            output = tops.run_show_cmds([bash_cmd])
-            logger.info(
-                "On device %s, output of %s command is: \n%s\n",
-                tops.dut_name,
-                bash_cmd,
-                output,
-            )
-            actual_output = {"dns_request_successful": True}
-
-        except (AssertionError, pyeapi.eapilib.CommandError):
-            actual_output = {"dns_request_successful": False}
-
-        return actual_output
-
     dut_parameters = tests_tools.parametrize_duts(TEST_SUITE, test_defs, dut_objs)
     test_duts = dut_parameters["test_dns_base_services"]["duts"]
     test_ids = dut_parameters["test_dns_base_services"]["ids"]
@@ -74,6 +38,7 @@ class DnsBaseServicesTests:
         self.output = ""
         test_params = tops.test_parameters["dns_name_server_check"]
         tops.actual_output = {}
+        tops.expected_output = {}
 
         # Forming output message if test result is passed
         tops.output_msg = "IPv4 and IPv6 name servers are configured on the device."
@@ -93,46 +58,42 @@ class DnsBaseServicesTests:
             self.output += f"\n\nOutput of {tops.show_cmd} command is: \n{output}"
 
             # Skipping testcase if SSO protocol is not configured on the device.
-            if not output.get("nameServerConfigs"):
+            version_verification = [element for element in [test_params.values()] if bool(element)]
+            if all(list(version_verification)[0]):
                 pytest.skip(
                     f"Name server is not configured on {tops.dut_name}, hence skipping the"
                     " testcase."
                 )
 
-            # Separating out v4 and v6 name servers
-            v4_name_servers = output["v4NameServers"]
-            v6_name_servers = output["v6NameServers"]
-
-            tops.actual_output.update(
-                {
-                    "ipv4_name_server_details": self.verify_dns(
-                        v4_name_servers,
-                        list(test_params.keys())[0],
-                        test_params["ipv4_fail_if_not_configured"],
-                        dut,
-                        tops,
+            try:
+                for ip_version_verification, verification_status in test_params.items():
+                    if verification_status and "ipv4" in ip_version_verification:
+                        ip_version = ip_version_verification.split("_")[0]
+                        vx_name_servers = output["v4NameServers"]
+                    elif verification_status and "ipv6" in ip_version_verification:
+                        ip_version = ip_version_verification.split("_")[0]
+                        vx_name_servers = output["v6NameServers"]
+                    tops.expected_output.update({f"{ip_version}_dns_request_successful": True})
+                    reverse_resolution_ip = vx_name_servers[0]
+                    bash_cmd = f"bash timeout 10 nslookup {reverse_resolution_ip}"
+                    bash_cmd_output = tops.run_show_cmds([bash_cmd])
+                    logger.info(
+                        "On device %s, output of %s command is: \n%s\n",
+                        tops.dut_name,
+                        bash_cmd,
+                        bash_cmd_output,
                     )
-                }
-            )
+                    tops.actual_output.update({f"{ip_version}_dns_request_successful": True})
 
-            tops.actual_output.update(
-                {
-                    "ipv6_name_server_details": self.verify_dns(
-                        v6_name_servers,
-                        list(test_params.keys())[1],
-                        test_params["ipv4_fail_if_not_configured"],
-                        dut,
-                        tops,
-                    )
-                }
-            )
+            except pyeapi.eapilib.CommandError:
+                tops.actual_output.update({f"{ip_version}_dns_request_successful": False})
 
             # Forming the output message if the testcase is failed
             if tops.actual_output != tops.expected_output:
                 tops.output_msg = "\n"
-                for name_server, server_details in tops.actual_output.items():
-                    ip_version = name_server.split("_")[0]
-                    if not server_details["dns_request_successful"]:
+                for name_server_details, dns_status in tops.actual_output.items():
+                    ip_version = name_server_details.split("_")[0]
+                    if not dns_status:
                         tops.output_msg += (
                             f"DNS request is not successful for {ip_version} version.\n"
                         )

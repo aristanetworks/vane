8cb0663d04486ee89605b9cad1b65a01aed37bb3
dakshta <dakshta.amilkanthwar@gslab.com>
2023-09-14 13:45:27 +0530

    Issue-443 updated logger with f-strings

--- nrfu_tests/test_system_hardware_cpu_idle.py
+++ nrfu_tests/test_system_hardware_cpu_idle.py
@@ -7,11 +7,11 @@ Testcase for verification of system hardware CPU idle time
 
 import pytest
 from pyeapi.eapilib import EapiError
-from vane.logger import logger
 from vane.config import dut_objs, test_defs
-from vane import tests_tools
+from vane import tests_tools, test_case_logger
 
 TEST_SUITE = "nrfu_tests"
+logging = test_case_logger.setup_logger(__file__)
 
 
 @pytest.mark.system
@@ -46,11 +46,8 @@ class CpuIdleTimeTests:
             expected range.
             """
             output = dut["output"][tops.show_cmd]["json"]
-            logger.info(
-                "On device %s, output of %s command is:\n%s\n",
-                tops.dut_name,
-                tops.show_cmd,
-                output,
+            logging.info(
+                f"On device {tops.dut_name}, output of {tops.show_cmd} command is:\n{output}\n"
             )
             self.output += f"Output of {tops.show_cmd} command is:\n{output}\n"
             cpu_idle_details = output.get("cpuInfo").get("%Cpu(s)").get("idle")
@@ -76,10 +73,8 @@ class CpuIdleTimeTests:
 
         except (AttributeError, LookupError, EapiError) as excep:
             tops.output_msg = tops.actual_output = str(excep).split("\n", maxsplit=1)[0]
-            logger.error(
-                "On device %s, Error while running testcase is:\n%s",
-                tops.dut_name,
-                tops.actual_output,
+            logging.error(
+                f"On device {tops.dut_name}, Error while running testcase is:\n{tops.actual_output}"
             )
 
         tops.test_result = tops.actual_output == tops.expected_output

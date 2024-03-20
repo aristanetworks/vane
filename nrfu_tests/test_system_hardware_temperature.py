462d24934f26268d91add4464581f6c7e22f6479
dakshata <dakshata.amilkanthwar@gslab.com>
2024-03-20 17:38:06 +0530

    Issue_63 updated tops.output_msg

--- nrfu_tests/test_system_hardware_temperature.py
+++ nrfu_tests/test_system_hardware_temperature.py
@@ -169,8 +169,8 @@ class SystemHardwareTemperatureTests:
 
             # Skipping test case if the device is vEOS.
             model = self.version_output.get("modelName")
-            if "vEOS" in model or "7010" in model:
-                tops.output_msg = f"Skipping test case on {tops.dut_name} as it is vEOS device."
+            if "vEOS" in model:
+                tops.output_msg = f"{tops.dut_name} is {model} device, hence test skipped."
                 tests_tools.post_process_skip(
                     tops, self.test_system_temperature_sensors, self.output
                 )
